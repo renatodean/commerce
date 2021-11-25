0fdc267806ce299a41cd8b2b22d6d8fc98d3ca15
Renato Dean <renato_dean@hotmail.com>
2021-11-25 19:58:58 -0300

    Testing update name.

--- framework/local/api/operations/get-site-info.ts
+++ framework/local/api/operations/get-site-info.ts
@@ -24,13 +24,13 @@ export default function getSiteInfoOperation({}: OperationContext<any>) {
       categories: [
         {
           id: 'new-arrivals',
-          name: 'New Arrivals',
+          name: 'Pachita\'s site',
           slug: 'new-arrivals',
           path: '/new-arrivals',
         },
         {
           id: 'featured',
-          name: 'Featured',
+          name: 'Compra todo',
           slug: 'featured',
           path: '/featured',
         },
